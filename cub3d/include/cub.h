16a0ceef8ac25f4cfa8afd8705ff49553b5d03bd
CABARBAYE Adrien <cabarbaye.adrien@gmail.com>
2024-10-16 18:39:50 +0200

    ajouts de structures

--- cub3d/include/cub.h
+++ cub3d/include/cub.h
@@ -6,7 +6,7 @@
 /*   By: acabarba <acabarba@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 17:39:46 by acabarba          #+#    #+#             */
-/*   Updated: 2024/10/02 18:42:03 by acabarba         ###   ########.fr       */
+/*   Updated: 2024/10/16 18:32:31 by acabarba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,15 +25,33 @@
 # define EAST 'E'
 # define WEST 'W'
 
+// infos des textures
+typedef struct s_info
+{
+	char	*path_north;
+	char	*path_south;
+	char	*path_east;
+	char	*path_west;
+}	t_info;
+
+// infos de la map
 typedef struct s_map //peut etre pour diviser les infos passé par une liste chainé ?
 {
 	char	**tab;
+	int		width;
+	int		heinght
 }	t_map;
+
+// structure principale 
+typedef struct s_game
+{
+	t_info	*infos;
+	t_map	*map;
+	// ajout mlx
+	// ajouter les controles
+}	t_game;
 
 void	main_error(char *str);
 void	main_mapcheck(t_map *map);
-void	check_mapcontent(t_map *map);
-
-
 
 #endif
--- cub3d/srcs/02_parsing.c
+++ cub3d/srcs/02_parsing.c
@@ -6,7 +6,7 @@
 /*   By: acabarba <acabarba@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 17:43:20 by acabarba          #+#    #+#             */
-/*   Updated: 2024/10/02 18:41:20 by acabarba         ###   ########.fr       */
+/*   Updated: 2024/10/16 17:29:52 by acabarba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,6 @@
 void	main_mapcheck(t_map *map)
 {
 	check_infomap(map);			//verif qu'il y est bien toutes les infos de map 1 : adresses des textures / 2. code couleur floor et celing / 3. la map
-	check_mapcontent(map);		//verif si que des 0 1 N S E W (attention que la map, pas les infos de la map)
+	check_mapcontent(map);		//verif si que des 0 1 N0 S E W ou des espaces a l'exterieur de la map (attention que la map, pas les infos de la map)
 	check_wall(map);			//verif que la map est entouré de 1
-}
-
-void	check_mapcontent(t_map *map)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (map->tab[i])
-	{
-		j = 0;
-		while (map->tab[i][j] != '\0')
-		{
-			if (map->tab[i][j] != WALL
-				&& map->tab[i][j] != GROUND
-				&& map->tab[i][j] != NORTH
-				&& map->tab[i][j] != SOUTH
-				&& map->tab[i][j] != EAST
-				&& map->tab[i][j] != WEST
-				&& map->tab[i][j] != '\n'
-				&& map->tab[i][j] != '\r')
-				main_error("map content");
-			j++;
-		}
-		i++;
-	}
 }
